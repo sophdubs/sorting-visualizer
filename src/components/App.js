7d1a8d29fffa33c559169be85013bd2b6d52ddcd
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-03-05 16:58:23 -0800

    Setting up page layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import "../styles/styles.css";
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Test</h1>
+            <div className="main-container">
+                <header>
+                    <h1>{`<h1>Sorting Algorithm Visualizer</h1>`}</h1>
+                </header>
+                <main>
+                    <section id="graph">
+
+                    </section>
+                    <section id="algorithm-list">
+
+                    </section>
+                    <section id="info">
+
+                    </section>
+                </main>
             </div>
         );
     };
