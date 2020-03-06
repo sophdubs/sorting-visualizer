0bf072746d63e130180683b0ab72f11d9b5a566d
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-03-05 17:02:56 -0800

    Started working on bars component

---
+++ src/components/Bars.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Bars extends React.Component {
+    render() {
+        return (
+            <div className="bars">
+                <ul className="bar-list">
+
+                </ul>
+            </div>
+        );
+    };
+}
+
+export default Bars;
