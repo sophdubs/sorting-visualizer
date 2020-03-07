9bb71bd59bd62721c70826cb6d33a24cba1f6e8e
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-03-06 18:03:45 -0800

    Added state and updating state when sorting algorithm is selected

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,30 @@ import Graph from "../components/Graph";
 import "../styles/layout.css";
 import "../styles/styles.css";
 
+import { data } from "../data";
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            selected: 'default',
+            speed: 50
+        }
+    }
+
+    handleClick = (e) => {
+        const selected = e.target.dataset.type;
+        this.setState({selected: selected});
+    }
+
+    handleSort = () => {
+        console.log('SORTING');
+    }
+
+    componentDidMount() {
+        console.log(this.state);
+    }
+  
     render() {
         return (
             <div className="main-container">
@@ -16,47 +39,45 @@ class App extends React.Component {
                     <section className="graph-section">
                         <Graph></Graph>
                         <div className="buttons">
-                            <button className="sort-button"><span className="purple">{`<button>`}</span>SORT<span className="purple">{`</button>`}</span></button>
+                            <button className="sort-button" onClick={this.handleSort}><span className="purple">{`<button>`}</span>SORT<span className="purple">{`</button>`}</span></button>
                             <button className="reset-button"><span className="purple">{`<button>`}</span>RESET<span className="purple">{`</button>`}</span></button>
                             <input type="range" min="1" max="100" value="50" className="slider"/>
                         </div>
                     </section>
                     <section className="algorithm-list-section">
-                        <h2><span className="yellow">{`<h2>`}</span>Select A Sorting Algorithm<span className="yellow">{`</h2>`}</span></h2>
-                        <h3><span className="green">{`<h3>`}</span>Selection: NULL<span className="green">{`</h3>`}</span></h3>
+                        <h2><span className="yellow">{`<h2>`}</span>Select a Sorting Algorithm<span className="yellow">{`</h2>`}</span></h2>
+                        <h3><span className="green">{`<h3>`}</span>Selection: {data[this.state.selected].type === 'Algorithm' ? 'NULL' : data[this.state.selected].type}<span className="green">{`</h3>`}</span></h3>
                         <div className="algorithm-uls">
                             <ul className="algorithm-ul-left">
-                                <li><span className="pink">{`<li>`}</span>Bubble Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Quick Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Merge Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Insertion Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="bubbleSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Bubble Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="quickSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Quick Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="mergeSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Merge Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="insertionSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Insertion Sort<span className="pink">{`</li>`}</span></li>
                             </ul>
                             <ul className="algorithm-ul-right">
-                                <li><span className="pink">{`<li>`}</span>Selection Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Heap Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Bucket Sort<span className="pink">{`</li>`}</span></li>
-                                <li><span className="pink">{`<li>`}</span>Count Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="selectionSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Selection Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="heapSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Heap Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="bucketSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Bucket Sort<span className="pink">{`</li>`}</span></li>
+                                <li data-type="countSort" onClick={this.handleClick}><span className="pink">{`<li>`}</span>Count Sort<span className="pink">{`</li>`}</span></li>
                             </ul>
                         </div>
 
                     </section>
                     <section className="info-section">
                         <div className="description">
-                            <h4><span className="dark-blue">{`<h4>`}</span>Algorithm Description:<span className="dark-blue">{`</h4>`}</span></h4>
-                            <p><span className="orange">{`<p>`}</span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores ratione dolor ut recusandae at consequuntur quo iure architecto doloribus, quam eius fugiat, iusto nihil. Possimus a error mollitia quam autem!<span className="orange">{`</p>`}</span></p>
+                            <h4><span className="dark-blue">{`<h4>`}</span>{data[this.state.selected].type} Description:<span className="dark-blue">{`</h4>`}</span></h4>
+                            <p><span className="orange">{`<p>`}</span>{data[this.state.selected].description}<span className="orange">{`</p>`}</span></p>
                         </div>
                         <div className="runtimes">
                             <div className="run-time">
-                            <h4><span className="dark-blue">{`<h4>`}</span>Algorithm Run Time:<span className="dark-blue">{`</h4>`}</span></h4>
-                                <p><span className="orange">{`<p>`}</span>Lorem ipsum,  error mollitia quam autem!<span className="orange">{`</p>`}</span></p>
+                            <h4><span className="dark-blue">{`<h4>`}</span>{data[this.state.selected].type} Run Time:<span className="dark-blue">{`</h4>`}</span></h4>
+                                <p><span className="orange">{`<p>`}</span>{data[this.state.selected].runtime}<span className="orange">{`</p>`}</span></p>
                             </div>
                             <div className="space-complexity">
-                            <h4><span className="dark-blue">{`<h4>`}</span>Algorithm Space Complexity:<span className="dark-blue">{`</h4>`}</span></h4>
-                                <p><span className="orange">{`<p>`}</span>Lorem ipsum,  error mollitia quam autem!<span className="orange">{`</p>`}</span></p>
+                            <h4><span className="dark-blue">{`<h4>`}</span>{data[this.state.selected].type} Space Complexity:<span className="dark-blue">{`</h4>`}</span></h4>
+                                <p><span className="orange">{`<p>`}</span>{data[this.state.selected].space}<span className="orange">{`</p>`}</span></p>
                             </div>
                         </div>
-
-
                     </section>
                 </main>
             </div>
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const data = {
+    default: {
+        type: 'Algorithm',
+        description: 'A sorting algorithm is a method for reorganizing a large number of items into a specific order, such as alphabetical, highest-to-lowest value or shortest-to-longest distance. Sorting algorithms take lists of items as input data, perform specific operations on those lists and deliver ordered arrays as output.',
+        runtime: 'An algorithm\'s run time is essentially a measure of its efficiency based on how long it takes to process a given input',
+        space: 'Space complexity is a measure of the amount of working storage an algorithm needs.'
+    },
+    quickSort: {
+        type: 'Quick Sort',
+        description: '',
+        runtime: '',
+        space:''
+    },
+    insertionSort: {
+        type: 'Insertion Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    bubbleSort: {
+        type: 'Bubble Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    selectionSort: {
+        type: 'Selection Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    heapSort: {
+        type: 'Heap Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    bucketSort: {
+        type: 'Bucket Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    countSort: {
+        type: 'Count Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    },
+    mergeSort: {
+        type: 'Merge Sort',
+        description: '',
+        runtime: '',
+        space: ''
+    }
+};
